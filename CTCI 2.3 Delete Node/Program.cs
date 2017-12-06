2ef867c906e268b2119bbda4d69d7324b3c20e3f
Qubert <upafourth@gmail.com>
2017-12-06 23:56:38 +0100

    Add project files.

---
+++ CTCI 2.3 Delete Node/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_2._3_Delete_Node
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
